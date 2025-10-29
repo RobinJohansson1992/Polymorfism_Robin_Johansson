012c983b4d38aadb59705ad09e49dcc18b771ecd
RobinJohansson1992 <robin-johansson@live.com>
2025-10-29 21:10:37 +0100

    Added geometry and sub classes. Also created objects in main and put them in an array.

---
+++ Polymorfism_Robin_Johansson/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorfism_Robin_Johansson
+{
+    internal class Circle : Geometry
+    {
+        public double Radius { get; set; }
+
+        public Circle()
+        {
+            Radius = 5;
+        }
+
+        public override double Area()
+        {
+            return Radius * Radius * Math.PI; 
+        }
+    }
+}
