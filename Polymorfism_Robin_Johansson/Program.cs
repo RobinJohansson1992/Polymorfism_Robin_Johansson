6d8b19a6985de7ed9f7025b91b5554f7c8d04cd7
RobinJohansson1992 <robin-johansson@live.com>
2025-10-29 19:50:56 +0100

    Add project files.

---
+++ Polymorfism_Robin_Johansson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymorfism_Robin_Johansson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
